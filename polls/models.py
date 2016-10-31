f2ab1143f9b309dc09abf34ff3ee1d7729fe3f6d
Pavel Cerny <cerny.pav@gmail.com>
2016-10-31 13:04:51 +0200

    own function for model object

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # to change models:
 # https://docs.djangoproject.com/en/1.10/intro/tutorial02/#activating-models
@@ -13,6 +15,10 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    # my own function
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
