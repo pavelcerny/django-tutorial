744520e61cc26758994e18262141bd7d8295300a
Pavel Cerny <cerny.pav@gmail.com>
2016-11-02 15:56:54 +0200

    User, habit, occurence model

--- habit_tracker/models.py
+++ habit_tracker/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user_name
+
+
+class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    habit_name = models.CharField(max_length=200)
+    repetitions_per_week = models.IntegerField(default=7)
+    volume_with_units = models.CharField(max_length=200)
+    starting_date = models.DateTimeField('date_started')
+
+    def __str__(self):
+        return self.habit_name + " " + self.repetitions_per_week + " " + self.volume_with_units
+
+
+class Occurrence(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    date = models.DateTimeField('date_recorded')
+
+    def __str__(self):
+        return self.date
+
+
+
+
