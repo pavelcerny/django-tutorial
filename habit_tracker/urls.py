fd9bfc960c86b9041ab49fd1c8af1fd0eb5e25e4
Pavel Cerny <cerny.pav@gmail.com>
2016-11-28 13:42:04 +0200

    comments to urls.py

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -5,8 +5,10 @@ from . import views
 app_name = 'habit_tracker'
 
 urlpatterns = [
-    # ex: /polls/
+    # ex: /habit_tracker/
     url(r'^$', views.habits, name='habits'),
+    # ex: /habit_tracker/statistics
     url(r'^statistics$', views.statistics, name='statistics'),
+    # ex: /habit_tracker/about
     url(r'^about$', views.about, name='about'),
 ]
