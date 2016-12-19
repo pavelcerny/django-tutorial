b19a729203ab2085bce5685b5b4b233f2d7d8439
Pavel Cerny <cerny.pav@gmail.com>
2016-12-19 13:39:33 +0200

    statistics done

--- tracker/templatetags/mainpage_filters.py
+++ tracker/templatetags/mainpage_filters.py
@@ -1,3 +1,4 @@
+import math
 from django import template
 from tracker.views import HabitItem
 from django.utils import timezone
@@ -31,8 +32,17 @@ def date_formated(date):
     else:
         return day + "." + month
 
+@register.filter()
+def format_days_elapsed(date):
+    days = date.days
+    weeks = math.ceil(days/7)
+
+    if days < 7:
+        return str(days) + " days"
+    else:
+        return str(weeks) + " weeks"
 
 @register.filter
 def format_elapsed_time_statistics(date):
     days = str(date.days)
-    return  days
+    return days
